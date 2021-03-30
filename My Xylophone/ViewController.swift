a42c0473d77a6096d6016da625e2c139af5ca39a
reydaniel <reynaldidaniel@outlook.com>
2021-03-30 23:43:15 +0700

    Create interface of xylophone and coding some function to play it sound

--- My Xylophone/ViewController.swift
+++ My Xylophone/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            
+        }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("Playing note \(sender.currentTitle!)")
+        sender.alpha = 0.7
+        playNote(noteName: sender.currentTitle!)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+            print("End")
+        }
     }
-
-
+    func playNote(noteName: String) {
+        let url = Bundle.main.url(forResource: noteName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 

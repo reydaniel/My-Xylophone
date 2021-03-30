d40f2e8349c1dfffaf9a773e63975031a4ec3d15
reydaniel <reynaldidaniel@outlook.com>
2021-03-30 23:49:55 +0700

    Update commit

--- My Xylophone/ViewController.swift
+++ My Xylophone/ViewController.swift
@@ -12,19 +12,17 @@ class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
     
-    override func viewDidLoad() {
-            super.viewDidLoad()
-            
-        }
-    
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("Playing note \(sender.currentTitle!)")
+//        print("Playing note \(sender.currentTitle!)")
+        
+        //when tapped, set alpha or opacity to 0.7
         sender.alpha = 0.7
         playNote(noteName: sender.currentTitle!)
         
+        //set delay within 0.2s before set the opacity back to 1
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
             sender.alpha = 1
-            print("End")
+//            print("End")
         }
     }
     func playNote(noteName: String) {
